7844b5d1e14fea42f827dca9dc6c598df27fed69
Gee <edozygeorge@gmail.com>
2020-12-26 19:21:40 +0100

    modified to scrape all available graphics card

--- app.py
+++ app.py
@@ -1,51 +1,54 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = f'https://www.newegg.com/p/pl?d=graphics+card'
+for page in range(1, 101):
+    # sets website url
+    my_url = f'https://www.newegg.com/p/pl?d=Graphics+card&page={page}'
 
-# Opens up connection grabbing d page
-uClient = uReq(my_url)
+    # Opens up connection grabbing the page
+    uClient = uReq(my_url)
 
-# offloads d content into a variable
-page_html = uClient.read()
+    # offloads d content into a variable
+    page_html = uClient.read()
 
-# closes d client
-uClient.close()
+    # closes d client
+    uClient.close()
 
-# calls bs4 fn to parse the file
-page_soup = soup(page_html, "html.parser")
+    # calls bs4 fn to parse the file
+    page_soup = soup(page_html, "html.parser")
 
-# grabs each product
-containers = page_soup.findAll("div", {"class": "item-cell"})
+    # grabs each product
+    containers = page_soup.findAll("div", {"class": "item-cell"})
+
 
 # saves file in csv format
-filename = "graphics_card.csv"
+filename = "graphics_card_all.csv"
 f = open(filename, "w")
 
 headers = "BRAND, PRODUCT NAME, SHIPPING\n"
 
 f.write(headers)
 
-# loops through each product in the container
-for container in containers:
-    # gets brand name
-    links = container.findAll("a", {"class": "item-brand"})
-    for link in links:
-        brand = link.img["title"]
 
-    # gets product name
-    title_container = container.findAll("a", {"class": "item-title"})
-    for title in title_container:
-        product_name = title.text
+for page in range(1, 101):
+    # loops through each product in the container
+    for container in containers:
+        # gets brand name
+        links = container.findAll("a", {"class": "item-brand"})
+        for link in links:
+            brand = link.img["title"]
 
-    # gets shipping price
-    shipping_container = container.findAll("li", {"class": "price-ship"})
-    for shipping in shipping_container:
-        if shipping.text == "":
-            shipping_price = "Not Available"
-        else:
-            shipping_price = shipping.text.strip()
+        # gets product name
+        title_container = container.findAll("a", {"class": "item-title"})
+        for title in title_container:
+            product_name = title.text
 
-    f.write(brand + "," + product_name.replace(",", " |") + "," + shipping_price + "\n")
+        # gets shipping price
+        shipping_container = container.findAll("li", {"class": "price-ship"})
+        for shipping in shipping_container:
+            if shipping.text == "":
+                shipping_price = "Not Available"
+            else:
+                shipping_price = shipping.text.strip()
 
-
+        f.write(brand + "," + product_name.replace(",", " |") + "," + shipping_price + "\n")
